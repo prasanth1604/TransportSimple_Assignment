b19f1752a712f1d63f2bc28ccf84754995c4f5b0
prasanth1604 <prasanthpadavala1@gmail.com>
2025-04-09 21:24:19 +0530

    first commit

---
+++ askme/askme/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path , include
+from django.conf import settings
+from django.conf.urls.static import static
+from myapp import views
+
+urlpatterns = [
+    path('',views.ques_list),
+    path('admin/', admin.site.urls),
+    path('quora/',include('myapp.urls'),name = 'quora'),
+    path('',include('django.contrib.auth.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
